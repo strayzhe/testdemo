9b7cc2ab47022fe3560dedeac792bcb8d967ec70
strayzhe <zhe_919865299@163.com>
2020-09-01 11:39:00 +0800

    测试项目

--- src/main/java/com/example/testdemo/controller/TestController.java
+++ src/main/java/com/example/testdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.testdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class TestController {
     @PostMapping("diot")
     public String test(){
         return "success";
+    }
+
+    @GetMapping("get")
+    public String test2(){
+        return "success get";
     }
 
 }
